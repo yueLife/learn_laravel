81dfdb34a06621401210024af2497e9835eca631
Zhou Yuejian <yueLife_1993@163.com>
2020-04-01 20:33:18 +0800

    修改布局

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,18 +6,39 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
+    /**
+     * 网站首页
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author: Zyj
+     * @date: 2020/4/1 15:47
+     */
     public function home()
     {
-        return view("StaticPages/home");
+        return view("static_pages/home");
     }
 
+    /**
+     * 网站关于页面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author: Zyj
+     * @date: 2020/4/1 15:47
+     */
     public function about()
     {
-        return view("StaticPages/about");
+        return view("static_pages/about");
     }
 
+    /**
+     * 网站帮助页面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author: Zyj
+     * @date: 2020/4/1 15:47
+     */
     public function help()
     {
-        return view("StaticPages/help");
+        return view("static_pages/help");
     }
 }
