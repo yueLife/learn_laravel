925c3b962350fe7171df0876d28e551baa622d58
Zhou Yuejian <yueLife_1993@163.com>
2020-04-02 21:57:35 +0800

    1.增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -17,4 +17,13 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return ;
+    }
 }
