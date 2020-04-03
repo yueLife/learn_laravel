18b368c5253bb4acfc37a7b32888066b1b37a30e
Zhou Yuejian <yueLife_1993@163.com>
2020-04-03 22:14:00 +0800

    1.添加heroku 数据库

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,11 +9,9 @@
     @include('layouts._header')
 
     <div class="container">
-        <div class="offset-md-1 col-md-10">
-            @include('shared._messages')
-            @yield('content')
-            @include('layouts._footer')
-        </div>
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
     </div>
 
     @include('layouts._footer_js')
