182f41ebf8f5798376bb2c8f737de3c3a6f13e1a
Zhou Yuejian <yueLife_1993@163.com>
2020-04-04 06:37:55 +0000

    1.管理员删除用户权限

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store']
+            'except' => ['show','create','store','index']
         ]);
 
         $this->middleware('guest',[
@@ -19,7 +19,8 @@ class UsersController extends Controller
         ]);
     }
 
-    public function index(){
+    public function index()
+    {
         $users = User::paginate(10);
         return view('users.index',compact('users'));
     }
@@ -76,5 +77,13 @@ class UsersController extends Controller
 
         session()->flash('success','个人资料更新成功！');
         return redirect()->route('users.show',$user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户！');
+        return back();
     }
 }
