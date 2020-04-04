bab78f953216c979fc25c67a81d6a3ba221f0e4d
Zhou Yuejian <yueLife_1993@163.com>
2020-04-04 00:28:55 +0000

    1.用户登录创建session

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,16 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+# 用户
+Route::get('/register','UsersController@create')->name('register');
+Route::resource('users','UsersController');
+
+# 登录
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
+
 # 微博
 Route::namespace('Weibo')->prefix('/weibo')->name('weibo.')->group(function () {
     Route::get('/', 'WeiboController@home')->name('home');
 });
-
-
-# 用户
-Route::get('/register','UsersController@create')->name('register');
-Route::resource('users','UsersController');
